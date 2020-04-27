2f4a0a38557422613abffd643b1c56605fec8f91
Damien T.D.K <damientohin@gmail.com>
2020-04-27 10:59:01 +0200

    I added zuul and eureka configurations to API gateway

--- src/back/apigateway/src/main/java/com/linktech/apigateway/apigateway/DemoApplication.java
+++ src/back/apigateway/src/main/java/com/linktech/apigateway/apigateway/DemoApplication.java
@@ -2,7 +2,11 @@ package com.linktech.apigateway.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy  // act as zuul proxy.
+@EnableEurekaServer //for making this application as eureka server.
 @SpringBootApplication
 public class DemoApplication {
 
