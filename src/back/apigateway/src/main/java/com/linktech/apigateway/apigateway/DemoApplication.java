5a7d0beb8a6e5465091a8f62e8803b177abda456
Damien T.D.K <damientohin@gmail.com>
2020-04-27 16:04:33 +0200

    registered microservices

--- src/back/apigateway/src/main/java/com/linktech/apigateway/apigateway/DemoApplication.java
+++ src/back/apigateway/src/main/java/com/linktech/apigateway/apigateway/DemoApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy  // act as zuul proxy.
-@EnableEurekaServer //for making this application as eureka server.
+@EnableZuulProxy
+@EnableEurekaServer
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
---
+++ src/back/run.sh
@@ -0,0 +1,17 @@
+
+ cd .apigateway/
+ mvn spring-boot:run
+
+cd ../userservice/
+ mvn spring-boot:run
+
+ cd ./postsservice/
+ mvn spring-boot:run
+
+
+ cd ../messagesservice/
+ mvn spring-boot:run
+ 
+ cd ./institutionsservice/
+ mvn spring-boot:run
+ 
