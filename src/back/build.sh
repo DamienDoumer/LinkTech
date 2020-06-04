eba39a757ff76adeeb87cbd9950ddbb8d7ec4281
scorsi <sylvain.corsini@protonmail.com>
2020-06-04 15:17:51 +0200

    Fix dockerfiles, docker-compose file and build/serve scripts

--- src/back/build.sh
+++ src/back/build.sh
@@ -1,6 +1,12 @@
 bash ./apigateway/build.sh 
+cp ./apigateway/DOCKERFILE_COPY ./apigateway/target/Dockerfile
 bash ./authservice/build.sh 
+cp ./authservice/DOCKERFILE_COPY ./authservice/target/Dockerfile
 bash ./postsservice/build.sh 
+cp ./postsservice/DOCKERFILE_COPY ./postsservice/target/Dockerfile
 bash ./userservice/build.sh 
+cp ./userservice/DOCKERFILE_COPY ./userservice/target/Dockerfile
 bash ./messagesservice/build.sh 
+cp ./messagesservice/DOCKERFILE_COPY ./messagesservice/target/Dockerfile
 bash ./institutionsservice/build.sh 
+cp ./institutionsservice/DOCKERFILE_COPY ./institutionsservice/target/Dockerfile
