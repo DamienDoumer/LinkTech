5fee56a33365b328c8c86cf550c5bbe8229297fe
scorsi <sylvain.corsini@protonmail.com>
2020-06-06 18:53:25 +0200

    Change Institutions components and add Profile components

--- src/front/src/config.ts
+++ src/front/src/config.ts
@@ -4,4 +4,5 @@ export const config = {
     apiUrl: apiUrl,
     authServiceUrl: 'http://localhost:3303/auth',
     institutionsServiceUrl: 'http://localhost:2002/institutions',
+    usersServiceUrl: 'http://localhost:2202/users',
 };
--- src/front/src/pages/InstitutionPage.tsx
+++ src/front/src/pages/InstitutionPage.tsx
@@ -1,16 +1,59 @@
-import React, {FunctionComponent} from "react";
+import React, {FunctionComponent, useCallback, useContext, useEffect, useState} from "react";
 
-import {useParams} from "react-router-dom";
+import axios from "axios";
+import {Link, useParams} from "react-router-dom";
 
+import {config} from "../config";
+import {AuthContext} from "../contexts/AuthContext";
+import {InstitutionModel} from "../models/InstitutionModel";
 import {MainLayout} from "../layouts/MainLayout";
-import {InstitutionComponent} from "../components/Institution/InstitutionComponent";
 
 export const InstitutionPage: FunctionComponent = () => {
+    const context = useContext(AuthContext);
+
     const {institutionId} = useParams();
+
+    const [institution, setInstitution] = useState<InstitutionModel | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    const fetchData = useCallback(() => {
+        if (context.state.token === undefined) return;
+
+        setLoading(true);
+
+        axios({
+            url: `${config.institutionsServiceUrl}/${institutionId}`,
+            method: 'GET',
+            headers: {Authorization: `Bearer ${context.state.token}`}
+        }).then((response) => {
+            setInstitution({
+                id: response.data.id,
+                name: response.data.name,
+                type: response.data.type,
+                location: response.data.location,
+                country: response.data.countryName,
+                sector: response.data.sector
+            });
+            setLoading(false)
+        }).catch((reason) => {
+            setError("");
+            setLoading(false);
+        })
+    }, [context.state.token, institutionId]);
+
+    useEffect(fetchData, [fetchData, context.state.token, institutionId]);
+
+    if (loading) return <MainLayout><p>Loading...</p></MainLayout>;
+    if (error || institution === null) return <MainLayout><p>Error!</p></MainLayout>;
 
     return (
         <MainLayout>
-            <InstitutionComponent institutionId={institutionId} />
+            <h1>Institution</h1>
+            <h2><Link to={`/institution/${institution.id}`}>{institution.name}</Link></h2>
+            <p>{institution.type}</p>
+            <p>{institution.country}, {institution.location}</p>
+            <p>{institution.sector}</p>
         </MainLayout>
-    )
+    );
 }
