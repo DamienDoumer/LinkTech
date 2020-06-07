fbb21b3a256f09a66c60345c16084478b054e92e
Damien T.D.K <damientohin@gmail.com>
2020-06-07 17:56:50 +0200

    fixed untracked files

--- src/back/institutionsservice/src/main/java/com/linktech/institutionsservice/institutionsservice/DemoApplication.java
+++ src/back/institutionsservice/src/main/java/com/linktech/institutionsservice/institutionsservice/DemoApplication.java
@@ -16,7 +16,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
+
     @Bean
     public CorsFilter corsFilter() {
     final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
